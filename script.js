54c84fecb6322d084cc1f4e61b95b52a8529a05b
maximilienmil <146172483+maximilienmil@users.noreply.github.com>
2023-10-13 15:18:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+function convertBinaryToDecimal() {
+    const binaryInput = document.getElementById('binaryInput').value;
+    const decimalOutput = document.getElementById('decimalOutput');
+    decimalOutput.textContent = parseInt(binaryInput, 2);
+}
+
+function convertDecimalToBinary() {
+    const decimalInput = document.getElementById('decimalInput').value;
+    const binaryOutput = document.getElementById('binaryOutput');
+    binaryOutput.textContent = (parseInt(decimalInput)).toString(2);
+}
