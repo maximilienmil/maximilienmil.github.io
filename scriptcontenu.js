c1b2f4e5f6c068dbe10f29ed68c81c2947cedc86
maximilienmil <146172483+maximilienmil@users.noreply.github.com>
2023-10-19 21:37:09 +0100

    fix contenu 3

--- scriptcontenu.js
+++ scriptcontenu.js
@@ -1,15 +1,16 @@
-const matrixContainer = document.querySelector('.matrix-container');
+const matrixContainer = document.getElementById("matrix-container");
 
 function createMatrixCharacter() {
-    const matrixCharacter = document.createElement('span');
+    const matrixCharacter = document.createElement("span");
     matrixCharacter.textContent = getRandomMatrixCharacter();
     matrixCharacter.style.left = `${getRandomNumber(0, 100)}%`;
     matrixCharacter.style.animationDuration = `${getRandomNumber(5, 15)}s`;
     matrixCharacter.style.animationDelay = `${getRandomNumber(0, 10)}s`;
     matrixCharacter.style.fontSize = `${getRandomNumber(10, 30)}px`;
-    matrixCharacter.classList.add('matrix');
+    matrixCharacter.style.opacity = `${getRandomNumber(0.4, 1)}`;
+    matrixCharacter.classList.add("matrix");
     matrixContainer.appendChild(matrixCharacter);
-    matrixCharacter.addEventListener('animationiteration', () => {
+    matrixCharacter.addEventListener("animationiteration", () => {
         matrixContainer.removeChild(matrixCharacter);
     });
 }
